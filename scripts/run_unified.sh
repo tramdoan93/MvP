f1838324300d70e3677972357dedb9414d5166d8
Tram Doan <62456573+tramdoan93@users.noreply.github.com>
2024-11-13 00:14:12 +0700

    change batch size

--- scripts/run_unified.sh
+++ scripts/run_unified.sh
@@ -28,7 +28,7 @@ python main.py \
     --multi_path \
     --num_path $INFER_PATH \
     --seed $SEED \
-    --train_batch_size 16 \
+    --train_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --learning_rate 1e-4 \
     --lowercase \
@@ -36,7 +36,7 @@ python main.py \
     --data_ratio 1.0 \
     --check_val_every_n_epoch 10  \
     --agg_strategy vote \
-    --eval_batch_size 64 \
+    --eval_batch_size 8 \
     --constrained_decode \
     --multi_task \
     --do_train \
